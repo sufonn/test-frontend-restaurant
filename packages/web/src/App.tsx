1719795a0c8eb7e7cbc7ff867d31891e6a5a1fa1
sufonn <surangkhanang.j@hotmail.com>
2025-01-25 06:55:52 +0700

    modify frontend

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,26 @@
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+import Watermark from "./components/atoms/Watermark"
+import Home from "./pages/Home/Home"
+
+const App = () => {
   return (
-    <div>
-      <h1>Edit this app to complete LINE MAN Wongnai Frontend Assignment!</h1>
+    <Watermark
+    content={`surangkhanang.jit`}
+    rotate={20}
+    width={360}
+    height={30}
+    gapX={300}
+    gapY={300}
+    fontSize={20}
+    fontColor="rgb(112 112 112 / 15%)"
+  >
+    <div className='main'>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
+    </Watermark>
   )
 }
 
---
+++ packages/web/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="flex justify-center gap-2">
+            <div></div> 
+            <div></div>
+        </div>
+    )
+}
+
+export default Home
