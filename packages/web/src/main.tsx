b086e8aa44dd54a34571c2d4a71c699c1957bfba
sufonn <surangkhanang.j@hotmail.com>
2025-01-29 11:12:53 +0700

    chore: update dependencies to latest versions and fix React 19 compatibility

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './styles/index.sass'
+import "./styles/index.sass";
 
 const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-      </BrowserRouter> 
+      </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
